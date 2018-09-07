e739fdb0a9f9ee7228f39b4a1ab097349ae866ad
Rick Morice <rick@floor9design.com>
2018-09-07 15:13:31 +0100

    Removing commented method.

--- src/DevelopmentSetup.php
+++ src/DevelopmentSetup.php
@@ -52,7 +52,7 @@ class DevelopmentSetup implements SetupInterface
         $this->createPostcodeNspls();
         $this->createUsertypes();
         $this->createOsgrdinds();
-        //$this->createOa11();
+        $this->createOa11();
         $this->createCounties();
         $this->createCeds();
         $this->createLauas();
